97e70f15adcbe5066b33e492afac560812c45158
nawedx <nawed04@gmail.com>
2018-10-22 12:13:00 +0530

    Extracts datewise RR generated

---
+++ helpful_scripts/datewise_invoice_per_rr.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd
+from pandas import ExcelWriter
+pd.set_option('display.max_columns', 10)
+
+def readAndClean(filename):
+	# Add skiprows if needed
+	df = pd.read_table(filename, skiprows=2)
+	df.columns = df.columns.str.replace(' ', '_')
+	df['INVOICE_NUMB'] = df['INVOICE_NUMB'].fillna(0).astype(int)
+	df = df[df.INVOICE_NUMB != 0]
+	#print(df)
+	print('Invoice Details Loaded\n')
+	return df
+
+def writeToExcel(df, i, filename='processed_invoice_details'):
+	writer = ExcelWriter(filename+str(i)+'.xls')
+	df.to_excel(writer,'Sheet1')
+	writer.save()
+
+def writeToTextfile(invoiceDates, rrGen, rrPending):
+    with open('rakes_loaded.txt', 'r') as f:
+        x = f.readlines()
+    with open("invoice_generated_datewise.txt", "w+") as text_file:
+        print("Sir, freight loading and RR issued on {}. Total Rake Loaded on Dt. {} is {}. RR issued on account of {} is {}. RR issued on account of {} is {}. RR issued on account of {} is {}. Pending RR on account of {} is {}. Pending RR on account of {} is {}. Pending RR on account of {} is {}. Regards".format(invoiceDates[2], invoiceDates[2], x[0], invoiceDates[2], rrGen[invoiceDates[2]], invoiceDates[1], rrGen[invoiceDates[1]], invoiceDates[0], rrGen[invoiceDates[0]], invoiceDates[2], rrPending[invoiceDates[2]], invoiceDates[1], rrPending[invoiceDates[1]], invoiceDates[0], rrPending[invoiceDates[0]]), file=text_file)
+
+if __name__ == '__main__':
+    filename = 'InvcDtls.xls'
+    df = readAndClean('/home/nawedx/Downloads/'+filename)
+    invoiceDates = sorted(df['INVOICE_DATE'].unique())
+    rrGen = {}
+    rrPending = {}
+    df['RR_NUMB'] = df['RR_NUMB'].fillna(0).astype(int)
+    for i in range(3):
+    	df1 = df[df.INVOICE_DATE == invoiceDates[i]]
+    	df2 = df1[df1.RR_DATE == invoiceDates[2]]    	
+    	rrGen[invoiceDates[i]] = df2.RR_DATE.count()
+    	df3 = df1[df1.RR_DATE > invoiceDates[2]]
+    	df4 = df1[df1.RR_NUMB == 0]
+    	rrPending[invoiceDates[i]] = df3['INVOICE_DATE'].count() + df4['INVOICE_DATE'].count()
+    	    
+    print('\nRR generated against dates : ')
+    print(rrGen)
+    print('\nRR pending against dates : ')
+    print(rrPending)
+    writeToTextfile(invoiceDates, rrGen, rrPending)
+    if os.path.exists('/home/nawedx/Downloads/'+filename):
+        os.rename('/home/nawedx/Downloads/'+filename, 'invcdtls_downloaded/InvcDtls_'+invoiceDates[0]+'---'+invoiceDates[2]+'.xls')
+    else:
+        print('File does not exist.\n')
+    
---
+++ helpful_scripts/invoice_details_download.py
@@ -0,0 +1,158 @@
+#Script to Download Invoice Details from RMS Queries of FOIS
+#Written by Nawed Imroze (nawedx)
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import time
+import os
+import pandas as pd
+from pandas import ExcelWriter
+pd.set_option('display.max_columns', 100)
+
+#Initializes the webdriver and starts the browser
+browser = webdriver.Firefox()
+browser.get("http://fois.indianrail.gov.in/rmsdqweb/view/GG_LoginMainPrtlRQ.jsp")
+#browser.maximize_window()
+
+#Checks until the Submit button has been loaded
+#I have made the assumption that if submit button is loaded all other fields before it has alse been loaded.
+login_attempt = None
+while not login_attempt:
+	try:
+		login_attempt = browser.find_element_by_xpath('//*[@id="Submit"]')
+	except NoSuchElementException:
+		time.sleep(1)
+
+#Finds the required login details
+username = browser.find_element_by_xpath('//*[@id="txtUserId"]')
+password = browser.find_element_by_xpath('//*[@id="txtPassword"]')
+location = browser.find_element_by_xpath('//*[@id="txtLocation"]')
+
+
+#The credentials for login has been kep in a different file and I won't upload it on github.
+#It will only be given to those who want to contribute to this project
+import credentials
+
+#Fits the login credentials and clicks on Submit
+username.send_keys(credentials.uname)
+password.send_keys(credentials.pwd)
+location.send_keys(credentials.loc)
+login_attempt.submit()
+time.sleep(3)
+login_attempt.submit()
+time.sleep(3)
+
+#Waits for the page and the elements to load and goes to required page i.e "Invoice Details"
+invc = None
+while not invc:
+	try:
+		invc = browser.find_element_by_xpath('//td[. = "Operations Control"]')
+	except NoSuchElementException:
+		time.sleep(1)
+invc.click()
+ftdetails = browser.find_element_by_xpath('//td[. = "Miscelleneous"]')
+ftdetails.click()
+ftdetails = browser.find_element_by_xpath('//td[. = "Invoice Details"]')
+ftdetails.click()
+
+#Switches to appropriate frame.
+frame2 = None
+while not frame2:
+	try:
+		frame2 = browser.find_element_by_xpath('//iframe[@name="frmInpt"]')
+	except NoSuchElementException:
+		time.sleep(1)
+browser.switch_to.frame(frame2)
+
+#Waits until Date range is selected and a key is pressed in the program to continue
+print('Please select proper date range and press any key to continue : ')
+inp = input()
+
+submitButton = None
+while not submitButton:
+	try:
+		submitButton = browser.find_element_by_id('Submit')
+	except NoSuchElementException:
+		time.sleep(1)
+submitButton.click()
+time.sleep(3)
+
+showAll = None
+while not showAll:
+	try:
+		showAll = browser.find_element_by_link_text('Show All')
+	except NoSuchElementException:
+		time.sleep(1)
+showAll.click()
+
+browser.switch_to.default_content()
+frm = browser.find_element_by_xpath('//iframe[@name="frmInpt"]')
+browser.switch_to.frame(frm)
+time.sleep(1)
+excelDown = browser.find_element_by_link_text('Excel')
+excelDown.click()
+
+#To confirm that Invoice Details is downloaded
+print('Please check whether InvcDtls.xls is downloaded and press any key : ')
+inp = input()
+
+browser.switch_to.default_content()
+
+invc = None
+while not invc:
+	try:
+		invc = browser.find_element_by_xpath('//td[. = "Managerial Set"]')
+	except NoSuchElementException:
+		time.sleep(1)
+invc.click()
+ftdetails = browser.find_element_by_xpath('//td[. = "Demand"]')
+ftdetails.click()
+ftdetails = browser.find_element_by_xpath('//td[. = "Area wise Loading/Unloading (Details)"]')
+ftdetails.click()
+
+frame2 = None
+while not frame2:
+	try:
+		frame2 = browser.find_element_by_xpath('//iframe[@name="frmInpt"]')
+	except NoSuchElementException:
+		time.sleep(1)
+browser.switch_to.frame(frame2)
+
+submitButton = None
+while not submitButton:
+	try:
+		submitButton = browser.find_element_by_id('Submit')
+	except NoSuchElementException:
+		time.sleep(1)
+submitButton.click()
+time.sleep(3)
+
+showAll = None
+while not showAll:
+	try:
+		showAll = browser.find_element_by_link_text('Show All')
+	except NoSuchElementException:
+		time.sleep(1)
+showAll.click()
+
+
+total = None
+while not total:
+	try:
+		total = browser.find_element_by_xpath('/html/body/center/div[2]/table/tbody/tr[1]/td[1]').text
+	except NoSuchElementException:
+		time.sleep(1)
+
+i=1
+while total != 'TOTAL':
+	total = browser.find_element_by_xpath('/html/body/center/div[2]/table/tbody/tr['+str(i)+']/td[1]').text
+	i = i + 1
+
+rakesLoaded = browser.find_element_by_xpath('/html/body/center/div[2]/table/tbody/tr['+str(i-1)+']/td[8]').text
+with open("rakes_loaded.txt", "w+") as text_file:
+	print("{}".format(rakesLoaded), file=text_file)
+
+inp = input()
+
+browser.quit()
