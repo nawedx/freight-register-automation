cf6a2bb06ab01151ad875362f6c029a457ecec97
nawedx <nawed04@gmail.com>
2019-01-03 23:54:34 +0530

    Refactors and makes generator code readable

--- helpful_scripts/message_generator.py
+++ helpful_scripts/message_generator.py
@@ -4,8 +4,9 @@ from pandas import ExcelWriter
 pd.set_option('display.max_columns', 10)
 #sys.stdout = open('runlog', 'w')
 
+downloadPath = '/home/nawedx/Downloads/'
+
 def readAndClean(filename):
-	# Add skiprows if needed
 	df = pd.read_table(filename, skiprows=2)
 	df.columns = df.columns.str.replace(' ', '_')
 	df['INVOICE_NUMB'] = df['INVOICE_NUMB'].fillna(0).astype(int)
@@ -22,57 +23,42 @@ def writeToExcel(df, i, filename='processed_invoice_details'):
 def writeToTextfile(invoiceDates, rrGen, rrPending):
     with open('rakes_loaded.txt', 'r') as f:
         numberRakeLoaded = f.read().splitlines()
+
     with open("invoice_generated_datewise.txt", "w+") as text_file:
         print("Sir, freight loading and RR issued on {}.".format(invoiceDates[2]), file=text_file)
         print("Total Rake Loaded on Dt. {} is {}.".format(invoiceDates[2], numberRakeLoaded[0]), file=text_file)
-        if rrGen[invoiceDates[2]] != 0:
-            print("RR issued on account of {} is {}.".format(invoiceDates[2], rrGen[invoiceDates[2]]), file=text_file)   
-        if rrGen[invoiceDates[1]] != 0:
-            print("RR issued on account of {} is {}.".format(invoiceDates[1], rrGen[invoiceDates[1]]), file=text_file) 
-        if rrGen[invoiceDates[0]] != 0:
-            print("RR issued on account of {} is {}.".format(invoiceDates[0], rrGen[invoiceDates[0]]), file=text_file) 
-        if rrPending[invoiceDates[2]] != 0:
-            print("Pending RR on account of {} is {}.".format(invoiceDates[2], rrPending[invoiceDates[2]]), file=text_file) 
-        if rrPending[invoiceDates[1]] != 0:
-            print("Pending RR on account of {} is {}.".format(invoiceDates[1], rrPending[invoiceDates[1]]), file=text_file) 
-        if rrPending[invoiceDates[0]] != 0:
-            print("Pending RR on account of {} is {}.".format(invoiceDates[0], rrPending[invoiceDates[0]]), file=text_file) 
+        for n in reversed(range(0, 3)):
+            if rrGen[invoiceDates[n]] != 0:
+                print("RR issued on account of {} is {}.".format(invoiceDates[n], rrGen[invoiceDates[n]]), file=text_file)   
+        for n in reversed(range(0, 3)):
+            if rrPending[invoiceDates[n]] != 0:
+                print("Pending RR on account of {} is {}.".format(invoiceDates[n], rrPending[invoiceDates[n]]), file=text_file)   
 
-if __name__ == '__main__':
-    #import invoice_details_download
+    print('\nMessage generated and written to text file.\n')
 
-    time.sleep(3)
-    filename = 'InvcDtls.xls'
-    df = readAndClean('/home/nawedx/Downloads/'+filename)
-    invoiceDates = sorted(df['INVOICE_DATE'].unique())
-    dates = [datetime.datetime.strptime(ts, "%d-%m-%y") for ts in invoiceDates]
-    dates.sort()
-    invoiceDates = [datetime.datetime.strftime(ts, "%d-%m-%y") for ts in dates]
-    print(invoiceDates)
-    
-    rrGen = {}
-    rrPending = {}
-    df['RR_NUMB'] = df['RR_NUMB'].fillna(0).astype(int)
-    for i in range(3):
-    	df1 = df[df.INVOICE_DATE == invoiceDates[i]]
-    	df2 = df1[df1.RR_DATE == invoiceDates[2]]    	
-    	rrGen[invoiceDates[i]] = df2.RR_DATE.count()
-    	df3 = df1[df1.RR_DATE > invoiceDates[2]]
-    	df4 = df1[df1.RR_NUMB == 0]
-    	rrPending[invoiceDates[i]] = df3['INVOICE_DATE'].count() + df4['INVOICE_DATE'].count()
-    	    
-    print('\nRR generated against dates : ')
-    print(rrGen)
-    print('\nRR pending against dates : ')
-    print(rrPending)
-    writeToTextfile(invoiceDates, rrGen, rrPending)
-    '''
-    if os.path.exists('/home/nawedx/Downloads/'+filename):
-        os.rename('/home/nawedx/Downloads/'+filename, 'invcdtls_downloaded/InvcDtls_'+invoiceDates[0]+'---'+invoiceDates[2]+'.xls')
-    else:
-        print('File does not exist.\n')
-    '''
+        
+filename = 'InvcDtls'
+df = readAndClean(downloadPath+filename+'.xls')
 
-    #import send_mail_position
-    #send_mail_position.sendd()
-    
+invoiceDates = sorted(df['INVOICE_DATE'].unique())
+dates = [datetime.datetime.strptime(ts, "%d-%m-%y") for ts in invoiceDates]
+dates.sort()
+invoiceDates = [datetime.datetime.strftime(ts, "%d-%m-%y") for ts in dates]
+print(invoiceDates)
+
+rrGen = {}
+rrPending = {}
+df['RR_NUMB'] = df['RR_NUMB'].fillna(0).astype(int)
+for i in range(3):
+    df1 = df[df.INVOICE_DATE == invoiceDates[i]]
+    df2 = df1[df1.RR_DATE == invoiceDates[2]]       
+    rrGen[invoiceDates[i]] = df2.RR_DATE.count()
+    df3 = df1[df1.RR_DATE > invoiceDates[2]]
+    df4 = df1[df1.RR_NUMB == 0]
+    rrPending[invoiceDates[i]] = df3['INVOICE_DATE'].count() + df4['INVOICE_DATE'].count()
+        
+print('\nRR generated against dates : ')
+print(rrGen)
+print('\nRR pending against dates : ')
+print(rrPending)
+writeToTextfile(invoiceDates, rrGen, rrPending)
